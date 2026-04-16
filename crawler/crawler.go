ec24c1119f8e3cea86a0ef5f44019562f3e762be
Denis Lysenko <un1audd@yandex.ru>
2026-04-16 15:47:33 +0300

    fix for linter check

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -245,10 +245,6 @@ func crawlPage(ctx context.Context, client *http.Client, limiter *rate.Limiter, 
 	report.Assets = []Asset{}
 
 	links := analyzePageContent(ctx, client, limiter, assetCache, resp.Body, opts.URL, &report, opts)
-	sort.Slice(report.Assets, func(i, j int) bool {
-		return report.Assets[i].URL < report.Assets[j].URL
-	})
-
 	seenBroken := make(map[string]bool)
 	uniqueBroken := []BrokenLink{}
 	for _, bl := range report.BrokenLinks {
